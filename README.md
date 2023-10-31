112e3273210bdcc78599a30d823d0026cafa544e
Sverre H. Huseby <shh@thathost.com>
2023-10-31 11:10:47 +0100

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# k3a-prompt-login-module
-A JAAS LoginModule intended for use with Kafka clients, asking for the user's password
+# prompt-login-module
+
+A JAAS `LoginModule` intended for use with Kafka clients. When using
+this module, it is not necessary to embed/store the password in the
+config. Instead, the user will be prompted for the password (and
+optionally, the username, if not present in the config).
+
+## How to use it
+
+Make the jar file available to the Kafka tools by installing it in the
+shared jar directory of the tools. This will typically be a
+`share/java/kafka` sub-directory under the base directory of the tools
+installation. If tools are installed in `/opt/kafka`, this command
+will install the jar:
+
+```shell
+KAFKA_BASEDIR=/opt/kafka
+PROMPT_LOGIN_MODULE_VERSION=1.1.0
+curl -sSfo ${KAFKA_BASEDIR}/share/java/kafka/prompt-login-module.jar \
+      https://github.com/statnett/k3a-prompt-login-module/releases/download/v${PROMPT_LOGIN_MODULE_VERSION}/k3a-prompt-login-module-${PROMPT_LOGIN_MODULE_VERSION}.jar
+```
+
+The latest version may be found [on GitHub](https://github.com/statnett/k3a-prompt-login-module).
+
+Reference the `io.statnett.k3a.authz.PromptLoginModule` class in
+the JAAS section of your Kafka properties file:
+
+```properties
+sasl.mechanism=PLAIN
+security.protocol=SASL_SSL
+sasl.jaas.config=io.statnett.k3a.authz.PromptLoginModule required \
+  username="my.user.name";
+```
+
+Run a Kafka command, referencing the Kafka properties, and note how it
+now asks for a password from the console (If the JAAS config lacks a
+username, it will be prompted for too):
+
+```shell
+$ kafka-topics --bootstrap-server broker:9092 --command-config kafka.properties --list
+Password:
+__consumer_offsets
+__transaction_state
+app_strm_demo_topic
+  :
+```
+
+If input or output is redirected, the password will not be read from
+the console, but instead a dialog window will open asking for the
+credentials.
---
+++ src/main/java/io/statnett/k3a/authz/PromptLoginModule.java
@@ -0,0 +1,166 @@
+package io.statnett.k3a.authz;
+
+import org.apache.kafka.common.security.plain.internals.PlainSaslServerProvider;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.spi.LoginModule;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.Console;
+import java.util.Map;
+
+/**
+ * A JAAS LoginModule for Kafka that will look for username and password configuration values,
+ * and prompt the user for any value that is missing.
+ *
+ * If a Java "Console" is available, ie. stdin/stdout is not redirected, the input
+ * will be read from the console. Otherwise, this class will try to disable
+ * "headless mode", and display a Swing dialog.
+ */
+public final class PromptLoginModule
+implements LoginModule {
+
+    private static final String USERNAME_CONFIG = "username";
+    private static final String PASSWORD_CONFIG = "password";
+    private Subject subject;
+    private String username;
+    private String password;
+
+    static {
+        System.setProperty("java.awt.headless", "false");
+        PlainSaslServerProvider.initialize();
+    }
+
+    @Override
+    public void initialize(final Subject subject, final CallbackHandler callbackHandler, final Map<String, ?> sharedState, final Map<String, ?> options) {
+        this.subject = subject;
+        username = (String) options.get(USERNAME_CONFIG);
+        password = (String) options.get(PASSWORD_CONFIG);
+        promoteNonNulls();
+    }
+
+    @Override
+    public boolean login() {
+        final Console console = System.console();
+        if (console != null) {
+            inputFromConsole(console);
+        } else {
+            inputFromWindow();
+        }
+        promoteNonNulls();
+        return true;
+    }
+
+    private void inputFromConsole(final Console console) {
+        if (username == null) {
+            username = console.readLine("Username: ");
+        }
+        if (password == null) {
+            final char[] chars = console.readPassword("Password: ");
+            if (chars != null) {
+                password = new String(chars);
+            }
+        }
+    }
+
+    private void inputFromWindow() {
+        new UsernamePasswordDialog();
+    }
+
+    @Override
+    public boolean logout() {
+        return true;
+    }
+
+    @Override
+    public boolean commit() {
+        return true;
+    }
+
+    @Override
+    public boolean abort() {
+        return false;
+    }
+
+    private void promoteNonNulls() {
+        if (username != null) {
+            subject.getPublicCredentials().add(username);
+        }
+        if (password != null) {
+            subject.getPrivateCredentials().add(password);
+        }
+    }
+
+    private final class UsernamePasswordDialog
+    extends JDialog {
+
+        UsernamePasswordDialog() {
+            super((Frame) null, "JAAS Login", true);
+            setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            final JPanel panel = new JPanel(new GridBagLayout());
+            panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+            final GridBagConstraints constraints = new GridBagConstraints();
+            constraints.fill = GridBagConstraints.HORIZONTAL;
+            constraints.insets = new Insets(2, 5, 2, 5);
+            final JTextField usernameInput = new JTextField(username == null ? "" : username, 20);
+            addInput(panel, constraints, 0, "Username", usernameInput);
+            final JPasswordField passwordInput = new JPasswordField(password == null ? "" : password, 20);
+            addInput(panel, constraints, 1, "Password", passwordInput);
+
+            final JButton submitButton = new JButton("Submit");
+            submitButton.addActionListener(e -> {
+                username = usernameInput.getText();
+                password = new String(passwordInput.getPassword());
+                dispose();
+            });
+            getRootPane().setDefaultButton(submitButton);
+            final JPanel buttonPanel = new JPanel();
+            buttonPanel.add(submitButton);
+
+            getContentPane().add(panel, BorderLayout.CENTER);
+            getContentPane().add(buttonPanel, BorderLayout.PAGE_END);
+
+            pack();
+            setResizable(false);
+            setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - getWidth()) / 2,
+                        (Toolkit.getDefaultToolkit().getScreenSize().height - getHeight()) / 2);
+            addWindowFocusListener(new WindowAdapter() {
+                @Override
+                public void windowGainedFocus(final WindowEvent e) {
+                    if (username != null) {
+                        passwordInput.requestFocusInWindow();
+                    }
+                }
+            });
+            setVisible(true);
+        }
+
+        private void addInput(final JPanel panel, final GridBagConstraints constraints, final int y, final String label, final Component input) {
+            constraints.gridy = y;
+            constraints.gridx = 0;
+            constraints.gridwidth = 1;
+            panel.add(new JLabel(label), constraints);
+            constraints.gridx = 1;
+            constraints.gridwidth = 2;
+            panel.add(input, constraints);
+        }
+
+    }
+
+}
